098f20c5de347b686cd62ad08a1eec5b7c1bfbb8
maicol <maicolmike@hotmail.com>
2023-10-27 12:00:00 -0500

    archivos base

--- peps/peps/views.py
+++ peps/peps/views.py
@@ -4,9 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html',{
-        'message': 'hola mundo desde el contexto',
-        'products': [
-            {'title':'playera', 'price':5,'stock':True },
-            {'title':'camisa', 'price':11,'stock':False },
-        ]
+        'title': "Index",
     })
