684cfd620f8854a44ea8f304a5c18b982dad66ab
maicol <maicolmike@hotmail.com>
2023-11-14 17:24:19 -0500

    se añadieron nuevos campos al modelo persona

--- peps/personas/models.py
+++ peps/personas/models.py
@@ -4,9 +4,16 @@ from django.db import models
 class PersonaPEP(models.Model):
     nombre = models.CharField(max_length=100)
     identificacion = models.CharField(max_length=20, unique=True)
+    es_pep = models.BooleanField(default=False)
+    estado = models.CharField(max_length=50)
+    tipo_pep = models.CharField(max_length=100)
     cargo = models.CharField(max_length=100)
+    fecha_vinculacion = models.DateField()
+    fecha_desvinculacion = models.DateField(null=True, blank=True)
+    cuentas_extranjeras = models.BooleanField(default=False)
     fecha_registro_pep = models.DateField()
-
+    fecha_actualizacion = models.DateField()
+    
     def __str__(self):
         return self.nombre
 
