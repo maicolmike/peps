e78e6a2a9445fa4d12408ab8ac0cc4912c76a20e
maicol <maicolmike@hotmail.com>
2023-11-27 16:02:02 -0500

    Se programo editar familiares pep

--- peps/personas/urls.py
+++ peps/personas/urls.py
@@ -1,9 +1,11 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('detallePep/<int:pk>/', views.PersonaPEPDetailView.as_view(), name='persona_detalle'),
-    path('crearPep/', views.crearPep, name='crear_pep'),
-    path('crearFamiliares/<int:persona_pep_id>/', views.crearFamiliares, name='crear_Familiares'),
-    path('editarPep/<int:pk>/', views.editarPep, name='editar_pep'),  # Nueva URL para la edición
+    path('crearPep/', views.crear_pep, name='crear_pep'),  # Cambiado aquí
+    path('crearFamiliares/<int:persona_pep_id>/', views.crear_familiares, name='crear_familiares'),  # Cambiado aquí
+    path('editarPep/<int:pk>/', views.editar_pep, name='editar_pep'),  
+    path('editarFamiliarPep/<int:familiar_id>/', views.editar_familiar_pep, name='editar_familiar_pep'),  # Cambiado aquí
 ]
--- peps/personas/views.py
+++ peps/personas/views.py
@@ -1,45 +1,31 @@
-from django.shortcuts import render,get_object_or_404
-
-
-# Create your views here.
-from django.views.generic import DetailView
-from .models import PersonaPEP,Familiar
-from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.decorators import login_required # vista basada en funciones que no permita acceder a paginas donde no se ha logeado
-from .forms import CrearPepForm,CrearFamiliaresForm
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin #vistas basada en clases
-from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.edit import UpdateView,DeleteView
 
-
+from django.views.generic import DetailView
+from .models import PersonaPEP, Familiar
+from .forms import CrearPepForm, CrearFamiliaresForm
 
 class PersonaPEPDetailView(DetailView):
     model = PersonaPEP
     template_name = 'personasPep/personaPep_detalle.html'
     context_object_name = 'persona'
-    
+
 @login_required(login_url='login')
-def crearPep(request):
+def crear_pep(request):
     form = CrearPepForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
         persona_pep = form.save()
         persona_pep.save()
         messages.success(request, 'Persona Pep creada con éxito')
+        return redirect(reverse('crear_familiares', kwargs={'persona_pep_id': persona_pep.id}))
 
-        # Redirigir a la página de creación de familiares para este PEP
-        return redirect(reverse('crear_Familiares', kwargs={'persona_pep_id': persona_pep.id}))
+    return render(request, 'personasPep/crearPep.html', {'title': "Crear pep", 'form': form})
 
-    return render(request, 'personasPep/crearPep.html', {
-        'title': "Crear pep",
-        'form': form
-    })
-    
 @login_required(login_url='login')
-def crearFamiliares(request, persona_pep_id):
+def crear_familiares(request, persona_pep_id):
     persona_pep = get_object_or_404(PersonaPEP, id=persona_pep_id)
     familiares = Familiar.objects.filter(persona_pep=persona_pep)
     form = CrearFamiliaresForm(request.POST or None)
@@ -49,7 +35,6 @@ def crearFamiliares(request, persona_pep_id):
         familiar.persona_pep = persona_pep
         familiar.save()
         messages.success(request, 'Familiar ha sido creado con éxito')
-        form = CrearFamiliaresForm()
 
     context = {
         'title': "Crear Familiares",
@@ -61,23 +46,26 @@ def crearFamiliares(request, persona_pep_id):
     return render(request, 'personasPep/crearFamiliares.html', context)
 
 @login_required(login_url='login')
-def editarPep(request, pk):
+def editar_pep(request, pk):
     persona_pep = get_object_or_404(PersonaPEP, pk=pk)
     form = CrearPepForm(request.POST or None, instance=persona_pep)
 
     if request.method == 'POST' and form.is_valid():
         form.save()
         messages.success(request, 'Persona Pep actualizada con éxito')
-
-        return redirect(reverse('crear_Familiares', kwargs={'persona_pep_id': persona_pep.pk}))
+        return redirect(reverse('crear_familiares', kwargs={'persona_pep_id': persona_pep.pk}))
 
     return render(request, 'personasPep/editarPep.html', {'title': 'Editar Pep', 'form': form, 'persona_pep': persona_pep})
 
-class ShippingAddressUpdateView(LoginRequiredMixin, SuccessMessageMixin,UpdateView):
-    login_url = 'login'
-    model = PersonaPEP
-    form_class = CrearPepForm
-    template_name = 'personasPep/editarPep.html'
-    
-    def get_success_url(self):
-        return reverse('shipping_addresses:shipping_addresses')
+@login_required(login_url='login')
+def editar_familiar_pep(request, familiar_id):
+    familiar = get_object_or_404(Familiar, id=familiar_id)
+    persona_pep = familiar.persona_pep
+    form = CrearFamiliaresForm(request.POST or None, instance=familiar)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        messages.success(request, 'Familiar de Persona Pep actualizada con éxito')
+        return redirect('crear_familiares', persona_pep_id=persona_pep.pk)
+
+    return render(request, 'personasPep/editarFamiliarPep.html', {'persona_pep': persona_pep, 'form': form, 'title': 'Editar Familiar Pep'})
