5e1c24d09734d9901059a505f42f43b00d73eee2
maicol <maicolmike@hotmail.com>
2024-01-23 16:53:02 -0500

    solucuion al error 404

--- peps/peps/settings.py
+++ peps/peps/settings.py
@@ -26,9 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s@*%i&oxy3v9sd3l)r94pkb31t%13jy$(w!jpzcx8pqvq72nk%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
